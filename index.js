d2c63cd9f73926c3cf22eb37beb0069e57bc4c97
joseph thomass <josephthomas88m@gmail.com>
2023-01-25 15:02:46 +0530

    Inintal commit Nav bar

---
+++ index.js
@@ -0,0 +1,27 @@
+let movie=[
+    {
+        name:"Falcon the winter soilder",
+        des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum minus incidunt maxime magni, vitae sit soluta veritatis aperiam inventore, mollitia fuga odio odit similique nesciunt facere laudantium neque doloribus.",
+
+        Image:"images/slider 2.png"
+    },
+    {
+        name:"Falcon the winter soilder",
+        des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum minus incidunt maxime magni, vitae sit soluta veritatis aperiam inventore, mollitia fuga odio odit similique nesciunt facere laudantium neque doloribus.",
+
+        Image:"images/slider 2.png"
+    },
+    {
+        name:"Falcon the winter soilder",
+        des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum minus incidunt maxime magni, vitae sit soluta veritatis aperiam inventore, mollitia fuga odio odit similique nesciunt facere laudantium neque doloribus.",
+
+        Image:"images/slider 2.png"
+    },
+    {
+        name:"Falcon the winter soilder",
+        des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eum minus incidunt maxime magni, vitae sit soluta veritatis aperiam inventore, mollitia fuga odio odit similique nesciunt facere laudantium neque doloribus.",
+
+        Image:"images/slider 2.png"
+    }
+
+]
